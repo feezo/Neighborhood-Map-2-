51acc69885027ea3154c67d29c227be677ec3ae9
feezo <jimohafis@yahoo.co.uk>
2018-12-06 15:47:23 +0000

    added axios

--- location-app/src/App.js
+++ location-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
 
@@ -9,11 +10,14 @@ componentDidMount(){
 
 renderMap = () => {
   loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCgugNkUxQKbCvivBo5_f8piPwxHINF3DM&callback=initMap")
+  // {convert initmap to a window object}
   window.initMap = this.initMap
 }
 
+// {initmap function copied from google maps api}
 initMap = () => {
   let map = new window.google.maps.Map(document.getElementById('map'), {
+    // {here we define google by adding window.}
           center: {lat: -34.397, lng: 150.644},
           zoom: 8
         });
@@ -28,6 +32,7 @@ initMap = () => {
   }
 }
 
+// {define a loadscript function to load script tag}
 function loadScript (url){
   let index = window.document.getElementsByTagName('script')[0]
   let script = window.document.createElement('script')
