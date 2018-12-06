5dc4c0dd18771ef0dfb02d1105a7cbfc79953fa7
feezo <jimohafis@yahoo.co.uk>
2018-12-06 17:09:06 +0000

    implementing foursquare api and axios

--- location-app/src/App.js
+++ location-app/src/App.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 class App extends Component {
 
 componentDidMount(){
+  // {invoke functions}
+  this.getVenues()
   this.renderMap()
 }
 
@@ -12,6 +14,25 @@ renderMap = () => {
   loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCgugNkUxQKbCvivBo5_f8piPwxHINF3DM&callback=initMap")
   // {convert initmap to a window object}
   window.initMap = this.initMap
+}
+
+// {create a for to fetch venue apis}
+getVenues = () => {
+  let endPoint = "https://api.foursquare.com/v2/venues/explore?"
+  let parameters = {
+    client_id:"S0KNYX5Z2ZEWDS5BQ0D5H5OMLXYFXH3HYEJG1K1QVPEPWXHA",
+    client_secret:"G3B1FVXX3CUS4SGJKHZP452F43WNOUSM1OFMFNJC5WWQFHBQ",
+    near:"Abuja" ,
+    query:"food",
+    v:"20180725"
+  }
+  axios.get(endPoint + new URLSearchParams(parameters))
+  .then(response => {
+    console.log(response)
+  })
+  .catch(error => {
+    console.log("Error !!!" + error);
+  })
 }
 
 // {initmap function copied from google maps api}
